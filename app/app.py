03bfe397a23a77cca1b04d01f2dd0d3140eef859
JexPy <guja1995@gmail.com>
2023-08-01 04:21:34 +0400

    * del # * formatted

--- app/app.py
+++ app/app.py
@@ -6,31 +6,32 @@ import logging
 import os
 
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='[%(asctime)s]: {} %(levelname)s %(message)s'
-                    .format(
-                        os.getpid()),
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[logging.StreamHandler()])
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(asctime)s]: {} %(levelname)s %(message)s".format(os.getpid()),
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler()],
+)
 
 logger = logging.getLogger()
 
 
 def create_app(config=default) -> Flask:
     app = Flask(__name__)
-    app.config.from_object(f'config.{config}')
-    # print(app.config)
+    app.config.from_object(f"config.{config}")
 
-    app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
-        'graphql',
-        schema=schema,
-        graphiql=True if app.config.get(
-            'FLASK_ENV') == 'development' else False
-    ))
+    app.add_url_rule(
+        "/graphql",
+        view_func=GraphQLView.as_view(
+            "graphql",
+            schema=schema,
+            graphiql=True if app.config.get("FLASK_ENV") == "development" else False,
+        ),
+    )
 
     return app
 
 
 if __name__ == "__main__":
     app = create_app(default)
-    app.run(debug=True, port=3000, host='0.0.0.0')
+    app.run(debug=True, port=3000, host="0.0.0.0")
